dac0ca5bec560e906f4150947c330f89881740cd
Muhammad Muntazir Mehdi <muhammad.muntazir@argonteq.com>
2025-09-08 18:15:00 +0500

    feat: learned how to use signed and unsigned data types

---
+++ hello_program/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let num: u16 = 65531;
+    println!("The number is: {}", num);
+    println!("Max u16 value is: {}", u8::MAX);
+    println!("Min u16 value is: {}", u8::MIN);
+    println!("Max u16 value is: {}", u16::MAX);
+    println!("Min u16 value is: {}", u16::MIN);
+    println!("Max u16 value is: {}", u32::MAX);
+    println!("Min u16 value is: {}", u32::MIN);
+    println!("Max u16 value is: {}", u64::MAX);
+    println!("Min u16 value is: {}", u64::MIN);
+    println!("Hello, world!");
+}
