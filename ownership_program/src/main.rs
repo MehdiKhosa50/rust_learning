139bd781533fa9535e8078afeb2aa7496f9c80b8
Muhammad Muntazir Mehdi <muhammad.muntazir@argonteq.com>
2025-09-09 19:03:41 +0500

    Understanding ownership with minimal example

--- ownership_program/src/main.rs
+++ ownership_program/src/main.rs
@@ -1,9 +1,30 @@
 fn main() {
-    println!("Hello, world!");
-    let str = String::from("Hello from ownership program!"
-    );
-    println!("{}", str);
-    let str2 = str; // ownership moved here
-    println!("{}", str2);
+
+    let my_string: String = get_string();
+    println!("{}", my_string);
+
+    let my_string2: String = String::from("Hello from main function!");
+    println!("{}", my_string2);
+
+    let my_string3: String = send_get_string(my_string2);
+    println!("{}", my_string3);
+    // println!("{}", my_string2); // this would cause a compile-time error because my_string2 is no longer valid
+
+
+    // println!("Hello, world!");
+    // let str = String::from("Hello from ownership program!"
+    // );
+    // println!("{}", str);
+    // let str2 = str; // ownership moved here
+    // println!("{}", str2);
     // println!("{}", str); // this would cause a compile-time error because str is no longer valid
+}
+
+fn get_string() -> String {
+    let owned_string = String::from("Hello from get_string function!");
+    return owned_string; // ownership moved to caller
+}
+
+fn send_get_string(received_string: String) -> String {
+    return received_string;
 }
