cbaf05b075f48f7b90b3cc670b1572360c2e3bf8
Muhammad Muntazir Mehdi <muhammad.muntazir@argonteq.com>
2025-09-10 20:30:59 +0500

    feat: calculate area of an rectangle.

--- struct_rust/src/main.rs
+++ struct_rust/src/main.rs
@@ -1,7 +1,14 @@
+mod rectangle;
+
 #[derive(Debug)]
 struct Point {
     x: i32,
     y: i32,
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
 fn main() {
@@ -10,4 +17,25 @@ fn main() {
     println!("Point: {:?}", p);
     println!("Point x: {}", p.x);
     println!("Point y: {}", p.y);
+
+    fn area(width: u32, height: u32) -> u32 {
+        width * height
+    }
+    println!("Area: {}", area(10, 300));
+
+    fn area2(rect: &Rectangle) -> u32 {
+        rect.width * rect.height
+    }
+    let rect = Rectangle {
+        width: 10,
+        height: 200,
+    };
+    println!("Rectangle area: {}", area2(&rect));
+
+    println!("Area: {}", rectangle::rectangle_area(30, 50));
+    let rect1 = Rectangle {
+        width: 10,
+        height: 200,
+    };
+    println!("Rectangle area: {}", rectangle::rectangle_area(rect1.width, rect1.height));
 }
---
+++ struct_rust/src/rectangle.rs
@@ -0,0 +1,3 @@
+pub fn rectangle_area(width: u32, height: u32) -> u32 {
+    width * height
+}
